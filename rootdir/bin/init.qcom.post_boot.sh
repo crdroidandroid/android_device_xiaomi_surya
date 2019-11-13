1fb17df5c368c1efbf96a9775708235ab45c2596
Kyle Lin <kylelin@google.com>
2019-11-13 11:41:12 +0800

    davinci: rootdir: Modify the cpuset setting
    Because the arch is similiar with SDM670, we aling the setting with
    bonito.
    
    Bug: 142552311
    Test: build , munaual check the settings
    Change-Id: Ica44125c8ed71a85cfd6bde77c9107bceed7b094

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -188,10 +188,10 @@ case "$target" in
 
     # cpuset parameters
     echo 0-7     > /dev/cpuset/top-app/cpus
-    echo 0-2,4-7 > /dev/cpuset/foreground/cpus
-    echo 0-2     > /dev/cpuset/background/cpus
-    echo 0-3     > /dev/cpuset/system-background/cpus
-    echo 0-3     > /dev/cpuset/restricted/cpus
+    echo 0-5,7 > /dev/cpuset/foreground/cpus
+    echo 4-5     > /dev/cpuset/background/cpus
+    echo 2-5     > /dev/cpuset/system-background/cpus
+    echo 2-5     > /dev/cpuset/restricted/cpus
 
     # Turn on sleep modes.
     echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
