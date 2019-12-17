e640620b1a5c300e85a2441a6adbca7a69f9651b
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2019-12-17 19:27:02 +0200

    surya: releasetools: Fix on incremental updates
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@gmail.com>
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -18,52 +18,52 @@ import common
 import re
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  input_zip = info.input_zip
+  OTA_InstallEnd(info, input_zip)
   return
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  input_zip = info.target_zip
+  OTA_InstallEnd(info, input_zip)
   return
 
-def AddImage(info, basename, dest):
+def AddImage(info, input_zip, basename, dest):
   name = basename
-  path = "IMAGES/" + name
-  if path not in info.input_zip.namelist():
-    return
+  data = input_zip.read("IMAGES/" + basename)
 
-def AddImageRadio(info, basename, dest):
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
+def AddImageRadio(info, input_zip, basename, dest):
   name = basename
-  path = "RADIO/" + name
-  if path not in info.input_zip.namelist():
-    return
+  data = input_zip.read("RADIO/" + basename)
 
-  data = info.input_zip.read(path)
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
-def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
-  AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+def OTA_InstallEnd(info, input_zip):
+  AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, input_zip, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
 
   # Firmware
-  AddImageRadio(info, "cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64")
-  AddImageRadio(info, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem")
-  AddImageRadio(info, "cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib")
-  AddImageRadio(info, "hyp.mbn", "/dev/block/bootdevice/by-name/hyp")
-  AddImageRadio(info, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth")
-  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz")
-  AddImageRadio(info, "aop.mbn", "/dev/block/bootdevice/by-name/aop")
-  AddImageRadio(info, "xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config")
-  AddImageRadio(info, "storsec.mbn", "/dev/block/bootdevice/by-name/storsec")
-  AddImageRadio(info, "uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp")
-  AddImageRadio(info, "imagefv.elf", "/dev/block/bootdevice/by-name/imagefv")
-  AddImageRadio(info, "qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw")
-  AddImageRadio(info, "abl.elf", "/dev/block/bootdevice/by-name/abl")
-  AddImageRadio(info, "dspso.bin", "/dev/block/bootdevice/by-name/dsp")
-  AddImageRadio(info, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster")
-  AddImageRadio(info, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg")
-  AddImageRadio(info, "xbl.elf", "/dev/block/bootdevice/by-name/xbl")
-  AddImageRadio(info, "ffu.img", "/dev/block/bootdevice/by-name/ffu")
+  AddImageRadio(info, input_zip, "cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64")
+  AddImageRadio(info, input_zip, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem")
+  AddImageRadio(info, input_zip, "cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib")
+  AddImageRadio(info, input_zip, "hyp.mbn", "/dev/block/bootdevice/by-name/hyp")
+  AddImageRadio(info, input_zip, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth")
+  AddImageRadio(info, input_zip, "tz.mbn", "/dev/block/bootdevice/by-name/tz")
+  AddImageRadio(info, input_zip, "aop.mbn", "/dev/block/bootdevice/by-name/aop")
+  AddImageRadio(info, input_zip, "xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config")
+  AddImageRadio(info, input_zip, "storsec.mbn", "/dev/block/bootdevice/by-name/storsec")
+  AddImageRadio(info, input_zip, "uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp")
+  AddImageRadio(info, input_zip, "imagefv.elf", "/dev/block/bootdevice/by-name/imagefv")
+  AddImageRadio(info, input_zip, "qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw")
+  AddImageRadio(info, input_zip, "abl.elf", "/dev/block/bootdevice/by-name/abl")
+  AddImageRadio(info, input_zip, "dspso.bin", "/dev/block/bootdevice/by-name/dsp")
+  AddImageRadio(info, input_zip, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster")
+  AddImageRadio(info, input_zip, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg")
+  AddImageRadio(info, input_zip, "xbl.elf", "/dev/block/bootdevice/by-name/xbl")
+  AddImageRadio(info, input_zip, "ffu.img", "/dev/block/bootdevice/by-name/ffu")
   return
