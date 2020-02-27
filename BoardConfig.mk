b9ffc1069f5912232cc9ef0cb6f60b0bf871692b
Peter Cai <peter@typeblog.net>
2020-02-27 21:13:21 +0800

    surya: Panic to recovery, not bootloader
    Change-Id: I614dae74dfe603fc2e9256b83e2c0dc72c846686
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,6 +128,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # Media
 TARGET_USES_ION := true
