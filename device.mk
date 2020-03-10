19526a7a578b8993d226828e5e226d992238528e
Jaegeuk Kim <jaegeuk@google.com>
2020-03-10 11:08:34 -0700

    surya: use metadata_csum for /metadata
    This enhances ext4 stability.
    
    Bug: 149391799
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: I8cebe30bdf66f39f0e4cc5955e53aca4d4f22f64

--- device.mk
+++ device.mk
@@ -345,6 +345,11 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    e2fsck_ramdisk \
+    tune2fs_ramdisk \
+    resize2fs_ramdisk
+
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
