b4d0891f767c348a3ec398ab31e6bd39cfe1bc0a
Demon000 <demonsingur@gmail.com>
2020-07-13 00:48:05 +0300

    surya: use dynamic partitions
    Change-Id: Iac89f608a7f8efa78da5200c68cb45ab2f439fd0

--- device.mk
+++ device.mk
@@ -317,6 +317,9 @@ PRODUCT_PACKAGES += \
     KarnaPowerProfile \
     NotchBarKiller
 
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
