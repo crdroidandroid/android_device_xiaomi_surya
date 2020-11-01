d3a1d816a45af608392480389de8dd85d8d734d6
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-01 17:51:18 -0500

    surya: Split component-overrides to support pure aosp/qti bt stack
    Change-Id: Ia40a996ba5164ba8a6fc5dde0dfb3ef859af1477
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- device.mk
+++ device.mk
@@ -92,7 +92,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
