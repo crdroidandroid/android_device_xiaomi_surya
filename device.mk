6434d03ee5c05dc5d304de520c5d2bde369f153e
luca020400 <luca.stefani.ge1@gmail.com>
2020-12-06 17:11:09 +0100

    surya: Set boot animation resolution
    Change-Id: Ib1a3d2a4ed498199920f196f4f69e03de1128d0d

--- device.mk
+++ device.mk
@@ -104,6 +104,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
