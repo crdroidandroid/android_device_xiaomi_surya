20a2fc011e93ed30997b4b4f61be5a022b59f617
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-16 14:29:25 +0530

    surya: Bring back megvii in camera HAL
     * required for face unlock
    
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,9 +59,6 @@ function blob_fixup() {
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
         vendor/lib64/hw/camera.qcom.so)
-            "${PATCHELF}" --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
-            "${PATCHELF}" --remove-needed "libmegface.so" "${2}"
-            "${PATCHELF}" --add-needed "libshim_megvii.so" "${2}"
             ;;
     esac
 }
