82494946c60582e7ea224c8c3d69da03f15cca88
Hridya Valsaraju <hridya@google.com>
2021-04-02 11:33:45 -0700

    surya: Set PRODUCT_SET_DEBUGFS_RESTRICTIONS
    Starting with Android R launched devices, debugfs cannot be mounted in
    production builds. In order to avoid accidental debugfs dependencies
    from creeping in during development with userdebug/eng builds, the
    build flag PRODUCT_SET_DEBUGFS_RESTRICTIONS can be set by vendors to
    enforce additional debugfs restrictions for userdebug/eng builds. The
    same flag will be used to enable sepolicy neveallow statements to
    prevent new permissions added for debugfs access.
    
    Test: build, boot
    Bug: 184381659
    Change-Id: I45e6f20c886d467a215c9466f3a09965ff897d7e

--- device.mk
+++ device.mk
@@ -247,6 +247,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
