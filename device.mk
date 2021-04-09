dea7ef4d76fe0b4597ee37d9cf530c24fb867648
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-09 22:17:10 +0530

    surya: Configure zram from separate fstab
    Change-Id: I3d093f06b08ad1cc049325d0136b60cab70c29d0

--- device.mk
+++ device.mk
@@ -177,7 +177,8 @@ PRODUCT_PACKAGES += \
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
