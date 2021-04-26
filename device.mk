571d95e785666b179774ac3204b0da55ae22f384
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-04-26 11:00:12 +0530

    surya: Add KeyHandler to handle side-mounted fingerprint key
    * This prevents the fingerprint sensor from triggering other events.
    
    Change-Id: I2b4543f2f02f974d52e96739569ba4aa9697c77e

--- device.mk
+++ device.mk
@@ -231,6 +231,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# Keyhandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
