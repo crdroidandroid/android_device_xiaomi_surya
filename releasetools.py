540c8d4a5b06264804e4f4c164fbfd5bd860381b
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-15 18:51:22 +0300

    surya: Ship FW with ROM
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- releasetools.py
+++ releasetools.py
@@ -31,6 +31,12 @@ def AddImage(info, basename, dest):
   if path not in info.input_zip.namelist():
     return
 
+def AddImageRadio(info, basename, dest):
+  name = basename
+  path = "RADIO/" + name
+  if path not in info.input_zip.namelist():
+    return
+
   data = info.input_zip.read(path)
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
@@ -40,4 +46,24 @@ def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+
+  # Firmware
+  AddImageRadio(info, "cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64")
+  AddImageRadio(info, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem")
+  AddImageRadio(info, "cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib")
+  AddImageRadio(info, "hyp.mbn", "/dev/block/bootdevice/by-name/hyp")
+  AddImageRadio(info, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth")
+  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz")
+  AddImageRadio(info, "aop.mbn", "/dev/block/bootdevice/by-name/aop")
+  AddImageRadio(info, "xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config")
+  AddImageRadio(info, "storsec.mbn", "/dev/block/bootdevice/by-name/storsec")
+  AddImageRadio(info, "uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp")
+  AddImageRadio(info, "imagefv.elf", "/dev/block/bootdevice/by-name/imagefv")
+  AddImageRadio(info, "qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw")
+  AddImageRadio(info, "abl.elf", "/dev/block/bootdevice/by-name/abl")
+  AddImageRadio(info, "dspso.bin", "/dev/block/bootdevice/by-name/dsp")
+  AddImageRadio(info, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster")
+  AddImageRadio(info, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg")
+  AddImageRadio(info, "xbl.elf", "/dev/block/bootdevice/by-name/xbl")
+  AddImageRadio(info, "ffu.img", "/dev/block/bootdevice/by-name/ffu")
   return
