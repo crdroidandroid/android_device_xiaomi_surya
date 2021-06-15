a6943514965807a28c1f7741859a7a7b21aac9ca
Dyneteve <sound0020@gmail.com>
2021-06-15 20:40:02 +0530

    surya: Set loop max part to 16
    * Cuz PSU wants to be like this
    
    Change-Id: I5be8c36a7198f1c9055d7ec93b33662f1e2e6738
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,7 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
-BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += loop.max_part=16
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # Media
