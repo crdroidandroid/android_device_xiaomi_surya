c1259df09e9f22b5cc4a37bbf7aac8fcb490f869
Unicote <arvogl874@gmail.com>
2021-07-21 13:47:06 +0300

    surya: overlay-lineage: Convert to overlay-evolution
    * Add new overlays
    * Also clean-up
    
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- device.mk
+++ device.mk
@@ -306,7 +306,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
