924ca38a287bf0d447f6becaad240834f1a52135
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-15 21:56:27 +0530

    surya: parts: Add support for LCD HBM and CABC
     * write preferences and qs tiles
     * kang icons from realmeparts
     * enable cabc by default
     * address sepolicy denials
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- device.mk
+++ device.mk
@@ -119,6 +119,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.xiaomiparts.rc
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.lcd.cabc_mode=1
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
