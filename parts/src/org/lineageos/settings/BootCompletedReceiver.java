ce61578ed00d2e24462682f34b03bfcb7588e9c7
GtrCraft <nickvanbokhorst@hotmail.com>
2021-08-15 20:00:45 +0200

    surya: Switch to crDroid doze package
    Change-Id: Ifb40bca56970bd200159f66c691be5533b74fdfc

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.haptic.HapticUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -35,7 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize();
-        DozeUtils.checkDozeService(context);
         HapticUtils.restoreLevel(context);
     }
 }
