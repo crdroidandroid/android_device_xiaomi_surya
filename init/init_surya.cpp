755f33bd087f027a22f46f9044bf5c9ebf9f4b4e
GtrCraft <nickvanbokhorst@hotmail.com>
2021-09-19 13:56:39 +0200

    surya: Update sunfish fingerprint to september patch
    Change-Id: I6598fecdc303a9528e839879dce6c4521c739954

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -100,8 +100,8 @@ void vendor_load_properties() {
         }
     }
 
-    fingerprint = "google/sunfish/sunfish:11/RQ3A.210805.001.A1/7474174:user/release-keys";
-    description = "sunfish-user 11 RQ3A.210805.001.A1 7474174 release-keys";
+    fingerprint = "google/sunfish/sunfish:11/RQ3A.210905.001/7511028:user/release-keys";
+    description = "sunfish-user 11 RQ3A.210905.001 7511028 release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
