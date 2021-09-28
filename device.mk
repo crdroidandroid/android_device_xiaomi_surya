e15972a390ef8b8bf45d808b860644706a7d2ebc
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-09-28 19:50:02 +0530

    surya: Set default Settings.Global.DEVICE_NAME for karna & surya
     * Reference:
       https://github.com/AdarshGrewal/android_device_xiaomi_sweet/commit/a4524e8d6648ec8e7f98e7d779d11eb98a0f5103
    
    Change-Id: I488a2b24df121766d9466b2516d7eb2ff14e4c81

--- device.mk
+++ device.mk
@@ -315,6 +315,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
+PRODUCT_PACKAGES += \
+    KarnaSettingsProviderOverlay \
+    SuryaSettingsProviderOverlay
+
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
