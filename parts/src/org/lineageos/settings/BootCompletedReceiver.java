79e53928aae9d10d4a16bb407d8925df125daa84
Lostark13 <tushar9915@gmail.com>
2022-01-20 14:54:47 +0530

    surya: parts: Add per app maximum refresh rate
    [1] - Base commits:
    - 9cd2ebf
    - 87a43c0
    - d795e4f
    
    Signed-off-by: Lostark13 <tushar9915@gmail.com>
    Signed-off-by: rdxzv <rdxzv.dev@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,6 +25,7 @@ import android.util.Log;
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -36,5 +37,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 	DiracUtils.initialize(context);
 	DozeUtils.checkDozeService(context);
         ThermalUtils.initialize(context);
+	RefreshUtils.startService(context);
     }
 }
