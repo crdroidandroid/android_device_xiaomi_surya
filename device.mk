2a82c8b1e370e5559c88e376bee13f9e6f0a38d0
Alexander Koskovich <akoskovich@pm.me>
2022-05-04 18:13:03 -0700

    surya: Drop some audio packages
     * All of these are implied.

--- device.mk
+++ device.mk
@@ -61,9 +61,6 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libvolumelistener
 
-PRODUCT_PACKAGES += \
-    libaudio-resampler
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
