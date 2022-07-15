b8c97c40a8774bd79cbb0b7a2e555202b1caec14
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-07-16 02:15:50 +0530

    surya: Add power profile for surya & karna
    Change-Id: Ic9ab02a69059451ef6250033b848c3ac0a71b9f4

--- device.mk
+++ device.mk
@@ -330,7 +330,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
 PRODUCT_PACKAGES += \
+    KarnaPowerProfile \
     KarnaSettingsProviderOverlay \
+    SuryaPowerProfile \
     SuryaSettingsProviderOverlay
 
 # Perf
