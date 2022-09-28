f747a4e7f95976bfb2981308fff5aed5c94e5c09
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-09-28 15:03:13 +0530

    surya: Provide android.hardware.drm@1.4 on vendor

--- device.mk
+++ device.mk
@@ -162,7 +162,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
