c17ad35d527a8de0bb233676b6888d3df656ec7b
Kuba Wojciechowski <nullbytepl@gmail.com>
2022-10-09 06:56:22 +0000

    surya: cmdline: Disable kpti
    * It can also help increase performance by a lot in some scenarios.
    
    Signed-off-by: kangeraarav <okand7946@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_KERNEL_IMAGE_NAME := Image.gz
