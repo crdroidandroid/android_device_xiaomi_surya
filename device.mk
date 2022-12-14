55adfccdcee8f1eb51cc275fa643a33bd13a78c9
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2022-12-14 09:08:45 -0600

    surya: build hardware audio 2.0-4.0
    [    7.888683] init: Command 'restart vendor.audio-hal-4-0-msd' action=onrestart (<Service 'audioserver' onrestart>:2) took 0ms and failed: service vendor.audio-hal-4-0-msd not found
    [    7.888698] init: Command 'restart audio_proxy_service' action=onrestart (<Service 'audioserver' onrestart>:3) took 0ms and failed: service audio_proxy_service not found
    [    7.888722] init: Command 'restart vendor.audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:4) took 0ms and failed: service vendor.audio-hal-2-0 not found
    [    7.888729] init: Command 'restart audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:5) took 0ms and failed: service audio-hal-2-0 not found

--- device.mk
+++ device.mk
@@ -43,6 +43,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
