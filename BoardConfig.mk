11221fb5635809f4296fd8d4147fd89e07d9d96c
TheStrechh <TheStrechh@gmail.com>
2023-01-15 16:56:32 +0000

    surya: Import qcom-caf FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
