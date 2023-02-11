7b9fbc8ed7516e6562003ce2901608bc0d8c4df1
thestrech <thestrechh@gmail.com>
2023-02-12 00:38:48 +0530

    surya: build libstdc++.vendor

--- device.mk
+++ device.mk
@@ -84,7 +84,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
-    libgui_vendor
+    libgui_vendor \
+    libstdc++.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
