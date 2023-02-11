9f6656f792a0869e4b1d21e9b6348f451a3ca226
thestrech <thestrechh@gmail.com>
2023-02-12 00:32:41 +0530

    surya: build libcamera2ndk_vendor

--- device.mk
+++ device.mk
@@ -82,6 +82,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
     libdng_sdk.vendor
 
 PRODUCT_COPY_FILES += \
