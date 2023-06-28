37e5db14ac1d9f4ada5f0226c8f44598b2724b9b
TheStrechh <TheStrechh@gmail.com>
2023-06-28 23:23:12 +0530

    surya: leica back

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -18,6 +18,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Leica Camera
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
