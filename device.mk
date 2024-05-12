0bfe053f9d23e10f887c4b0c84f2bb589565e9ca
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 08:59:13 +0300

    surya: Drop libvulkan target that is built by default
    Change-Id: Ic5d73508b850fac7c7427caf12eb4a8e87e3deca

--- device.mk
+++ device.mk
@@ -412,9 +412,6 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # Vulkan
-PRODUCT_PACKAGES += \
-    libvulkan
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
