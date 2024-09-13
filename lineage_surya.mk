72ec35fc9c1672b818972e58112c533693704769
shoya0x00 <shoya0x00@gmail.com>
2024-09-13 09:59:52 +0600

    surya: crDroid build flags
    * Enable blur
    * Exclude AudioFX (we got dolby)

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -16,6 +16,10 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# crDroid build flag
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_DISABLE_MATLOG := true
+
 PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
