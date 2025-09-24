3fc1a4e1f8e5503e9988e56c4a3deed7fad5ceb6
shoya0x00 <shoya0x00@gmail.com>
2025-09-24 12:33:11 +0600

    Revert "surya: Migrate sensor HAL to AIDL interface"
    This reverts commit 958891858c97cc77ab322fa2e64a7f95791988ff.

--- device.mk
+++ device.mk
@@ -336,7 +336,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.xiaomi-multihal
+    android.hardware.sensors@2.1-service.multihal
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
